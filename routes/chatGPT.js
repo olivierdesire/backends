a59ca7e138633a7ffd30738b3beeef5828d0ff3a
Olivier Désiré <olivierdesire@MacBook-Pro-de-Olivier.local>
2023-03-26 18:32:29 +0200

    chatGPT + cors

--- routes/chatGPT.js
+++ routes/chatGPT.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const router = express.Router();
 const { Configuration, OpenAIApi } = require("openai");
 const mongoose = require("mongoose");
